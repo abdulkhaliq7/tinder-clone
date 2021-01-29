372ff7f0f7ca2b082e42f612ec2f8c370c0e1384
abdulkhaliq7 <abdikhaliq_7@hotmail.com>
2021-01-29 20:46:33 +0800

    added heroku url

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,20 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './TinderCards.css';
 import TinderCard from 'react-tinder-card';
 import { SwipeableDrawer } from '@material-ui/core';
+import axios from './axios';
+
 
 
 function TinderCards() {
-    const [people, setPeople] = useState([
-        {
-            name: 'Elon Musk',
-            url:'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTY2MzU3Nzk2OTM2MjMwNTkx/elon_musk_royal_society.jpg'
-        },
-        {
-            name: 'Jeff bezos',
-            url:'https://pbs.twimg.com/profile_images/669103856106668033/UF3cgUk4_400x400.jpg'
+    const [people, setPeople] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const req = await axios.get('/tinder/cards')
+
+            setPeople(req.data)
         }
-    ]);
+
+        fetchData();
+    }, [])
+
+    console.log(people)
 
     const swiped = (direction, nameToDelete) => {
         console.log('removing: ' + nameToDelete);
@@ -37,7 +42,7 @@ function TinderCards() {
                         onCardLeftScreen={() => outOfFrame(person.name)}
                     >
                         <div
-                            style={{ backgroundImage: `url(${person.url})`}}
+                            style={{ backgroundImage: `url(${person.imgUrl})`}}
                             className='card'
                         >
                             <h3>{person.name}</h3>
